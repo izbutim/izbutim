222135b9e54e887abb624cbed22f19a6b2b5661c
Nándor <39672261+ncsereoka@users.noreply.github.com>
2022-07-12 20:32:47 +0300

    Add dummy pages for future work

---
+++ src/pages/consiliul-local.tsx
@@ -0,0 +1,23 @@
+import * as React from 'react';
+
+const pageStyles = {
+  color: '#232129',
+  padding: '96px',
+  fontFamily: '-apple-system, Roboto, sans-serif, serif',
+};
+const headingStyles = {
+  marginTop: 0,
+  marginBottom: 64,
+  maxWidth: 320,
+};
+
+const HistoricBuildingsPage = () => {
+  return (
+    <main style={pageStyles}>
+      <title>Consiliul local</title>
+      <h1 style={headingStyles}>Consiliul local</h1>
+    </main>
+  );
+};
+
+export default HistoricBuildingsPage;
