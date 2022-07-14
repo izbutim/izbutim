ece63f70e849976fd7e56fe0768029fe9dc7ea69
Nándor <39672261+ncsereoka@users.noreply.github.com>
2022-07-14 21:18:59 +0300

    Add Izbutim to maps

--- src/components/common/TimisoaraMap/TimisoaraMap.tsx
+++ src/components/common/TimisoaraMap/TimisoaraMap.tsx
@@ -15,7 +15,7 @@ const TimisoaraMap = (props: Props) => {
       style={{ minHeight: '100vh', minWidth: '100vw' }}
     >
       <TileLayer
-        attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+        attribution='Izbutim | &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
         url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
       />
       {props.children}
