cb8de9af956d9a493fde15d95095ccb24f12c880
Nándor <39672261+ncsereoka@users.noreply.github.com>
2022-07-15 19:34:37 +0300

    Solve issue with map height

--- src/components/common/TimisoaraMap/TimisoaraMap.tsx
+++ src/components/common/TimisoaraMap/TimisoaraMap.tsx
@@ -1,6 +1,7 @@
 import React, { ReactNode } from 'react';
 import { MapContainer, TileLayer } from 'react-leaflet';
 import 'leaflet/dist/leaflet.css';
+import * as styles from './TimisoaraMap.module.css';
 
 type Props = {
   children?: ReactNode;
@@ -12,7 +13,7 @@ const TimisoaraMap = (props: Props) => {
       center={[45.7565, 21.229]}
       zoom={15}
       scrollWheelZoom={false}
-      style={{ minHeight: '100vh', minWidth: '100vw' }}
+      className={styles.map}
     >
       <TileLayer
         attribution='Izbutim | &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
